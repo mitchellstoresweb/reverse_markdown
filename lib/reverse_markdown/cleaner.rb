0acfe4e9219ac59274a05275370f914804b455e2
Karlyn Murphy <kmurphy@thoughtworks.com>
2015-06-08 17:44:09 -0400

    [KM VS] cleaner#clean_tag_borders no longer adds unnecessary whitespaces

--- lib/reverse_markdown/cleaner.rb
+++ lib/reverse_markdown/cleaner.rb
@@ -25,30 +25,8 @@ module ReverseMarkdown
       end
     end
 
-    # Find non-asterisk content that is enclosed by two or
-    # more asterisks. Ensure that only one whitespace occurs
-    # in the border area.
-    # Same for underscores and brackets.
     def clean_tag_borders(string)
-      result = string.gsub(/\s?\*{2,}.*?\*{2,}\s?/) do |match|
-        preserve_border_whitespaces(match, default_border: ' ') do
-          match.strip.sub('** ', '**').sub(' **', '**')
-        end
-      end
-
-      result = result.gsub(/\s?\_{2,}.*?\_{2,}\s?/) do |match|
-        preserve_border_whitespaces(match, default_border: ' ') do
-          match.strip.sub('__ ', '__').sub(' __', '__')
-        end
-      end
-
-      result = result.gsub(/\s?~{2,}.*?~{2,}\s?/) do |match|
-        preserve_border_whitespaces(match, default_border: ' ') do
-          match.strip.sub('~~ ', '~~').sub(' ~~', '~~')
-        end
-      end
-
-      result.gsub(/\s?\[.*?\]\s?/) do |match|
+      string.gsub(/\s?\[.*?\]\s?/) do |match|
         preserve_border_whitespaces(match) do
           match.strip.sub('[ ', '[').sub(' ]', ']')
         end
--- spec/lib/reverse_markdown/cleaner_spec.rb
+++ spec/lib/reverse_markdown/cleaner_spec.rb
@@ -61,47 +61,10 @@ describe ReverseMarkdown::Cleaner do
   end
 
   describe '#clean_tag_borders' do
-    it 'removes not needed whitespaces from strong tags' do
-      input = "foo ** foobar ** bar"
-      result = cleaner.clean_tag_borders(input)
-      expect(result).to eq "foo **foobar** bar"
-    end
-
-    it 'remotes leading or trailing whitespaces independently' do
-      input = "1 **fat ** 2 ** fat** 3"
-      result = cleaner.clean_tag_borders(input)
-      expect(result).to eq "1 **fat** 2 **fat** 3"
-    end
-
-    it 'adds whitespaces if there are none' do
-      input = "1**fat**2"
-      result = cleaner.clean_tag_borders(input)
-      expect(result).to eq "1 **fat** 2"
-    end
-
     it "doesn't add whitespaces to underscore'ed elements if they are part of links" do
       input = "![im__age](sou__rce)"
       result = cleaner.clean_tag_borders(input)
       expect(result).to eq "![im__age](sou__rce)"
     end
-
-    it "still cleans up whitespaces that aren't inside a link" do
-      input = "now __italic __with following [under__scored](link)"
-      result = cleaner.clean_tag_borders(input)
-      expect(result).to eq "now __italic__ with following [under__scored](link)"
-    end
-
-    it 'cleans italic stuff as well' do
-      input = "1 __italic __ 2 __ italic__ 3__italic __4"
-      result = cleaner.clean_tag_borders(input)
-      expect(result).to eq "1 __italic__ 2 __italic__ 3 __italic__ 4"
-    end
-
-    it 'cleans strikethrough stuff as well' do
-      input = "1 ~~italic ~~ 2 ~~ italic~~ 3~~italic ~~4"
-      result = cleaner.clean_tag_borders(input)
-      expect(result).to eq "1 ~~italic~~ 2 ~~italic~~ 3 ~~italic~~ 4"
-    end
   end
-
 end
